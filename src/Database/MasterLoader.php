a785686823d665ce5a8604dc7b9dcccddadfaf1b
BangLipai <developer.kukuh@gmail.com>
2024-02-09 08:48:10 +0700

    Trait seeder master loader

---
+++ src/Database/MasterLoader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BangLipai\Utility\Database;
+
+use Arr;
+use DB;
+use Schema;
+
+trait MasterLoader
+{
+    protected function load(string $table): void
+    {
+        $directory = property_exists($this, 'masterPath') ? $this->masterPath : 'master';
+
+        $path = implode(DIRECTORY_SEPARATOR, array_filter([__DIR__, $directory, "$table.json"]));
+        $data = file_get_contents($path);
+        $json = json_decode($data, true);
+
+        $columns = Schema::getColumnListing($table);
+
+        foreach (array_chunk($json ?: [], 1000) as $data) {
+            $value = Arr::map($data, fn($item) => Arr::only($item, $columns));
+            DB::table($table)
+                ->insertOrIgnore($value);
+        }
+    }
+}
