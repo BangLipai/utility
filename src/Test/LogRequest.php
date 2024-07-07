9f1ddaf74d373ebad884e186c69a47b474525fea
BangLipai <developer.kukuh@gmail.com>
2024-07-07 22:35:38 +0700

    Setup helper test

---
+++ src/Test/LogRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BangLipai\Utility\Test;
+
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Routing\Events\Routing;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @mixin TestCase
+ */
+trait LogRequest
+{
+    public function setUpLogRequest(): void
+    {
+        Event::listen(function (Routing $event) {
+            if (config('app.debug')) {
+                Log::debug('req >> ' . $event->request->method() . ' ' . $event->request->fullUrl());
+            }
+        });
+
+        Event::listen(function (RequestHandled $event) {
+            if (config('app.debug')) {
+                Log::debug('resp << ' . $event->response->getStatusCode() . ' ' . (Response::$statusTexts[$event->response->getStatusCode()] ?? ''));
+            }
+        });
+    }
+}
