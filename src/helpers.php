a2d4b4eeeab8a2af9c972674b15877ba7946ce8f
BangLipai <developer.kukuh@gmail.com>
2024-07-21 20:08:00 +0700

    Helpers paginasi dan set aktif sidebar

--- src/helpers.php
+++ src/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
-
 use Illuminate\Http\JsonResponse;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 if (!function_exists('success')) {
     function success($data = null, $status = 200, $code = null, $meta = null): JsonResponse
@@ -14,5 +14,40 @@ if (!function_exists('error')) {
     function error($message = null, $data = null, int $status = 500, int $code = null, array $meta = null): JsonResponse
     {
         return Response::error($message, $data, $status, $code, $meta);
+    }
+}
+
+if (!function_exists('number_pagination')) {
+    function number_pagination(LengthAwarePaginator $collection, object $loop): string
+    {
+        return ($collection->currentpage() - 1) * $collection->perpage() + $loop->index + 1;
+    }
+}
+
+if (!function_exists('parse_no_wa')) {
+    function parse_no_wa(string $no_telp): string
+    {
+        return preg_replace('/^0/', '62', $no_telp);
+    }
+}
+
+if (!function_exists('prefixActive')) {
+    function prefixActive(string $prefixName): string
+    {
+        return request()->route()->getPrefix() == $prefixName ? 'active' : '';
+    }
+}
+
+if (!function_exists('prefixBlock')) {
+    function prefixBlock(string $prefixName): string
+    {
+        return request()->route()->getPrefix() == $prefixName ? 'block' : 'none';
+    }
+}
+
+if (!function_exists('routeActive')) {
+    function routeActive(string $routeName): string
+    {
+        return request()->routeIs($routeName) ? 'active' : '';
     }
 }
